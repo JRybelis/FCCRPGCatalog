38d7438feab34c142c35ff409809da0ff2c85bfd
Jacob <frizjr@protonmail.com>
2022-02-02 07:55:36 +0200

    Fixed dependencies for running the app

--- Startup.cs
+++ Startup.cs
@@ -12,53 +12,55 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace FCCRPGCatalog;
-
-public class Startup
+namespace FCCRPGCatalog
 {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-        Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        services.AddSwaggerGen(c =>
+        public Startup(IConfiguration configuration)
         {
-            c.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Title = "FCCRPFCatalog"
-                , Description = null
-                , Version = "Version 1"
-                , TermsOfService = null
-                , Contact = null
-                , License = null
-                , Extensions = null
-                ,
-            });
-        });
-    }
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Endpoints" ));
+            Configuration = configuration;
         }
-
-        app.UseHttpsRedirection();
-        app.UseRouting();
-        app.UseAuthorization();
-        app.UseEndpoints(endpoints =>
+    
+        public IConfiguration Configuration { get; }
+    
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-            endpoints.MapControllers();
-        });
-    } 
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FCCRPFCatalog"
+                    , Description = null
+                    , Version = "Version 1"
+                    , TermsOfService = null
+                    , Contact = null
+                    , License = null
+                    , Extensions = null
+                    ,
+                });
+            });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Endpoints" ));
+            }
+    
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        } 
+    }
 }
+
