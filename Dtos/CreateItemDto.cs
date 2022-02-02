e197120594371613bcf52b67d846e0734477d781
Jacob <frizjr@protonmail.com>
2022-02-02 22:29:28 +0200

    Added attributes to the CreateItemDto.cs to validate incoming Post requests

--- Dtos/CreateItemDto.cs
+++ Dtos/CreateItemDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FCCRPGCatalog.Dtos;
 
 public record CreateItemDto
 {
+    [Required]
     public string Name { get; init; }
+    [Required, Range(1, 5000)]
     public decimal Price { get; init; }
 }
