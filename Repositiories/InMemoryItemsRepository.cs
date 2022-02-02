49177eccd55a4ab915f4d2a64956e75adf5baead
Jokūbas Rybelis <jokubas.rybelis@softra.lt>
2022-02-02 17:44:11 +0200

    Updated items prices and added GetAll and GetById methods.

--- Repositiories/InMemoryItemsRepository.cs
+++ Repositiories/InMemoryItemsRepository.cs
@@ -1,4 +1,5 @@
 using FCCRPGCatalog.Entities;
+using System.Linq;
 
 namespace FCCRPGCatalog.Repositiories;
 
@@ -6,12 +7,22 @@ public class InMemoryItemsRepository
 {
     private readonly List<Item> items = new()
     {
-        new Item{Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreationDate = DateTimeOffset.UtcNow},
-        new Item{Id = Guid.NewGuid(), Name = "Iron Longsword", Price = 9, CreationDate = DateTimeOffset.UtcNow},
-        new Item{Id = Guid.NewGuid(), Name = "Bronze Dagger", Price = 9, CreationDate = DateTimeOffset.UtcNow},
-        new Item{Id = Guid.NewGuid(), Name = "Mithril Pickaxe", Price = 9, CreationDate = DateTimeOffset.UtcNow},
-        new Item{Id = Guid.NewGuid(), Name = "Adamant Helmet", Price = 9, CreationDate = DateTimeOffset.UtcNow},
-        new Item{Id = Guid.NewGuid(), Name = "Steel Chainmail", Price = 9, CreationDate = DateTimeOffset.UtcNow},
+        new Item{Id = Guid.NewGuid(), Name = "Potion", Price = 10, CreationDate = DateTimeOffset.UtcNow},
+        new Item{Id = Guid.NewGuid(), Name = "Iron Longsword", Price = 35, CreationDate = DateTimeOffset.UtcNow},
+        new Item{Id = Guid.NewGuid(), Name = "Bronze Dagger", Price = 15, CreationDate = DateTimeOffset.UtcNow},
+        new Item{Id = Guid.NewGuid(), Name = "Mithril Pickaxe", Price = 230, CreationDate = DateTimeOffset.UtcNow},
+        new Item{Id = Guid.NewGuid(), Name = "Adamant Helmet", Price = 280, CreationDate = DateTimeOffset.UtcNow},
+        new Item{Id = Guid.NewGuid(), Name = "Steel Chainmail", Price = 150, CreationDate = DateTimeOffset.UtcNow},
     };
+
+    public IEnumerable<Item> GetItems()
+    {
+        return items;
+    }
+
+    public Item GetItemById(Guid id)
+    {
+        return items.Where(item => item.Id == id).SingleOrDefault();
+    }
 
 }
